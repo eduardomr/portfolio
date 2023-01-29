06ee081aaddf8f62017c0db6ad60bd501b344a2c
Eduardo <emaiarezende@gmail.com>
2023-01-29 00:05:55 -0300

    criação de nova barra de navegação

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 
-
+import NavBar from './components/NavBar.jsx';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Home from './components/Home/Home';
-import QuemSouEu from './components/QuemSouEu/QuemSouEu';
-
 
 function App() {
-
   return (
     <div className="App">
-     <div className="menu"> <Navbar /></div>
-      <Home />
-    <QuemSouEu />
+      <NavBar/>
     </div>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+  render() {
+    return (
+        <nav>
+        <a href="#">Home</a>
+        <a href="#">Front-end skills</a>
+        <a href="#">Back-end skills</a>
+        <a href="#">Portfolio</a>
+        <a href="#">Contato</a>
+      </nav>
+    );
+  }
+}
+export default NavBar;
