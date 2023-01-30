13de177188848283a09d69a2a1b1f46c1295196f
Eduardo <emaiarezende@gmail.com>
2023-01-30 11:36:20 -0300

    barra de navegação não responsiva

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
+import logo from "../assets/logo.png";
 
 class NavBar extends React.Component {
   render() {
     return (
+      <>
+        <div className="logo">
+          <img src={logo} alt="logo" />
+        </div>
         <nav>
-        <a href="#">Home</a>
-        <a href="#">Front-end skills</a>
-        <a href="#">Back-end skills</a>
-        <a href="#">Portfolio</a>
-        <a href="#">Contato</a>
-      </nav>
+          <a href="#">Home</a>
+          <a href="#">Front-end skills</a>
+          <a href="#">Back-end skills</a>
+          <a href="#">Portfolio</a>
+          <a href="#">Contato</a>
+        </nav>
+      </>
     );
   }
 }
